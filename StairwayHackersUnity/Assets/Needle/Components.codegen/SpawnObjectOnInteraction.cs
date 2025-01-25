f13230a35176f44594592efecb912ab1e9d45169
Tobi <75667865+Alive-hub@users.noreply.github.com>
2025-01-24 19:17:07 -0500

    Reading the main scene to spawn objects at runtime

---
+++ StairwayHackersUnity/Assets/Needle/Components.codegen/SpawnObjectOnInteraction.cs
@@ -0,0 +1,16 @@
+// NEEDLE_CODEGEN_START
+// auto generated code - do not edit directly
+
+#pragma warning disable
+
+namespace Needle.Typescript.GeneratedComponents
+{
+	public partial class SpawnObjectOnInteraction : UnityEngine.MonoBehaviour
+	{
+		public string @myStringField = "Hello World";
+		public UnityEngine.UI.Button @debugButton;
+		public void start(){}
+	}
+}
+
+// NEEDLE_CODEGEN_END
